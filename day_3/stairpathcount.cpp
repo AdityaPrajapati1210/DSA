02ae999c2e367195dea02775fea719bd2b15b6a9
AdityaPrajapati1210 <aditya20051012@gmail.com>
2025-06-27 13:21:25 +0530

    Add files via upload

---
+++ day_3/stairpathcount.cpp
@@ -0,0 +1,15 @@
+// count the number of ways stairpath if single ,double and triple stair are allowed.
+#include <iostream>
+using namespace std;
+
+int stairpath(int n){
+    if(n==1 || n==2 || n==3) return n;
+    int total_ways = stairpath(n-1)+ stairpath(n-2)+ stairpath(n-3);
+    return total_ways;
+}
+
+int main() {
+   int n= 5;
+   int result = stairpath(n);
+   cout << result<< endl;
+}

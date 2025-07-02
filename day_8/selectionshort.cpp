f7845ecaef37771ddb8ff195ef3310d07db5fa92
Aditya Kumar Prajapati <aditya20051012@gmail.com>
2025-07-02 13:33:35 +0530

    Update selectionshort.cpp

--- day_8/selectionshort.cpp
+++ day_8/selectionshort.cpp
@@ -1,4 +1,4 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
 #include <algorithm>
 
@@ -7,16 +7,16 @@ using namespace std;
 int main() {
    int a[5]= {4,3,2,1,5};
    int n=5;
-   int minn = 999;
-   int minIndex;
    for(int i=0;i<n-1;i++){
+       int minn = 999;
+       int minIndex=-1;
        for(int j=i;j<n;j++){
            if(minn>a[j]){
-               minn = min(minn,a[j]);
+               minn = a[j];
                minIndex =j;
            }
-           swap(a[minIndex],a[i]);
-       }
+        }
+        swap(a[minIndex],a[i]);
    }
    for(int i=0;i<n;i++){
        cout << a[i] << "  ";
